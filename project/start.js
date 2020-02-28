959264b2748d2307c6d5e01bd843d1fce6985100
Jude I <jude.i@outlook.com>
2020-02-29 09:09:14 +1000

    Remove express and ejs

--- project/start.js
+++ project/start.js
@@ -7,11 +7,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname));
 
 // views is directory for all template files
-app.set('views', __dirname + '/html');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/html');
+//app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res, next) {
+  res.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
